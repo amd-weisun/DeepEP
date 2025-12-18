675cae2560e27b22ef67970106ef3c7389d7924f
amd-weisun <Wei.Sun@amd.com>
2025-12-18 16:41:59 +0000

    [wip] print the error message

--- tests/test_intranode_mori.py
+++ tests/test_intranode_mori.py
@@ -110,12 +110,15 @@ def test_main(num_sms: int, local_rank: int, num_ranks: int, rank: int, buffer: 
                         check_data(recv_x, rank_prefix_matrix)
                     if with_topk:
                         # Check `topk_idx`
+                        print(f'[debug] recv_topk_idx (rank {local_rank}):', recv_topk_idx.cpu(), flush=True)
+                        print(f'[debug] recv_num_tokens_per_expert_list (rank {local_rank}):', recv_num_tokens_per_expert_list, flush=True)
                         assert (recv_topk_idx.eq(-1) | ((recv_topk_idx >= 0) & (recv_topk_idx < (num_experts // num_ranks)))).sum().item() == recv_topk_idx.numel()
                         for i, count in enumerate(recv_num_tokens_per_expert_list):
                             assert recv_topk_idx.eq(i).sum().item() == count
 
                         # Check `topk_weights`
                         if current_x is not x_pure_rand:
+                            print(f'[debug] recv_topk_weights (rank {local_rank}):', recv_topk_weights.cpu(), flush=True)
                             recv_topk_weights[recv_topk_idx.eq(-1)] = recv_topk_weights.amax(dim=1, keepdim=True).expand_as(recv_topk_weights)[recv_topk_idx.eq(-1)]
                             check_data(recv_topk_weights, rank_prefix_matrix)
 
